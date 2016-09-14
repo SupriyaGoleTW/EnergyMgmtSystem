1d955674efdd1661032be7aec0ac2145dea4fddb
Shruti Chakraborty <shruti.chakraborty16@gmail.com>
2016-09-14 21:10:02 +0530

    Fixed the turning of option in automatic mode

--- Arduino/only_automatic/only_automatic.ino
+++ Arduino/only_automatic/only_automatic.ino
@@ -1,24 +1,31 @@
-int input_for_automate_button = 2; //number of input pin from which your button is connected
+int autoInput = 2; //number of input pin from which your button is connected
 int ledPin = 13;
 int relayOut = 12;
 
 void setup(){
   pinMode(ledPin,OUTPUT);
-  pinMode(input_for_automate_button,INPUT);
+  pinMode(autoInput,INPUT);
   pinMode(relayOut,OUTPUT);
   Serial.begin(9600);
 }
 
 void loop(){
   delay(500);
-  int value_of_pir_sensor = digitalRead(input_for_automate_button);
-  jumpToAutomaticMode(value_of_pir_sensor);
+  int sensorStatus = digitalRead(autoInput);
+  Serial.println(sensorStatus);
+  if(sensorStatus != HIGH){
+    turnOff();  
+  }
+  else{
+    turnOn(sensorStatus);
+  }
 }
 
 
-int bufferLight(int counter,int time,int inputValue){
+int generateBuffer(int time){
+  int counter = 0;
   while(counter < time){// turn led on till some limit take input from sensor -> need to work on that
-    int value_of_pir_sensor = digitalRead(inputValue);
+    int value_of_pir_sensor = digitalRead(2);
     if(value_of_pir_sensor == HIGH){
       Serial.println("resetted- again motion detected");
       counter = 0;
@@ -32,26 +39,26 @@ int bufferLight(int counter,int time,int inputValue){
   return counter;
 }
 
-void jumpToAutomaticMode(int value){
+void turnOn(int value){
   int timeLimit = 3900 * 0.1;
-  int counter = 0;
   
   Serial.println("In Automatic mode");
   while(value == HIGH){
     Serial.println("Motion Sensed");
-    int maxLimitReached = bufferLight(counter,timeLimit,input_for_automate_button);
+    int maxLimit = generateBuffer(timeLimit);
     
-    if(maxLimitReached == timeLimit){   // make LED low once time elapse
+    if(maxLimit == timeLimit){   // make LED low once time elapse
       delay(1000);
-      Serial.println("OFF LED");
-      digitalWrite(ledPin,LOW);
-      digitalWrite(relayOut,LOW);
+      turnOff();
     }
     return;
   }
- digitalWrite(ledPin,LOW);
- Serial.println("LED is Off");
- digitalWrite(relayOut,LOW);
- return;
+}
+
+
+void turnOff(){
+//  Serial.println("OFF LED");
+  digitalWrite(ledPin,LOW);
+  digitalWrite(relayOut,LOW);
 }
 
